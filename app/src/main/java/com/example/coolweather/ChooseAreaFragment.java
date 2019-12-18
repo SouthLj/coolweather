4ccacb7310e9730bd19b0b8a19e4c63628b33998
Foam <192783414@qq.com>
2019-12-18 08:56:20 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -82,6 +83,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currenrLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currenrLevel == LEVEL_COUNTY){
+                    String weatherId = countryList.get(position).getWeatherId();
+                    Intent intent  = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
